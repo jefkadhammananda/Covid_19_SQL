affe864e8f17dd5f8777d4ff818ea86ce4bcd436
Jefka Dhammananda <104891582+jefkadhammananda@users.noreply.github.com>
2022-05-14 18:10:19 +0700

    Update SQLCovid for Tableau.sql

--- SQLCovid for Tableau.sql
+++ SQLCovid for Tableau.sql
@@ -1,5 +1,6 @@
 
 --menjalankan tiap query dan melakukan copy table hasil query dan paste ke excel
+--pada query ini ganti nilai NULL yang ada menjadi nilai 0 agar tipe data nya menjadi numerik
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortofolioProject..CovidDeaths
@@ -11,8 +12,6 @@ Where continent is null
 and location not in ('World', 'European Union', 'International')
 Group by location
 order by TotalDeathCount desc
-
---pada query ini ganti nilai NULL yang ada menjadi nilai 0 agar tipe data nya menjadi numerik
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortofolioProject..CovidDeaths
